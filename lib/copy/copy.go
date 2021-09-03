22cf65a10643c0ce961c0019e66498d28ab23ae1
Daisuke Takayama <takayama_daisuke@cocone.co.jp>
2021-09-03 17:05:51 +0900

    refactor: 💡 copy func

--- lib/copy/copy.go
+++ lib/copy/copy.go
@@ -1,8 +1,6 @@
 package copy
 
 import (
-	"fmt"
-	"github.com/markbates/pkger"
 	"io"
 	"os"
 	"path"
@@ -11,17 +9,12 @@ import (
 func Start(targetPath string) error {
 	exePath, _ := os.Getwd()
 	targetFile := path.Join(exePath, targetPath)
-
-	fmt.Println("targetPath: ", targetPath)
-	fmt.Println("targetFile: ", targetFile)
 	stockPath := path.Join(exePath, "stock")
-	fmt.Println(stockPath)
 
-	f, err := pkger.Open(targetFile)
+	f, err := os.Open(targetFile)
 	if err != nil {
 		return err
 	}
-
 	defer f.Close()
 
 	info, err := f.Stat()
@@ -29,17 +22,10 @@ func Start(targetPath string) error {
 		return err
 	}
 
-	exe, err := os.Executable()
-	fmt.Println("exe: ", exe)
-	fmt.Println("Name: ", info.Name())
-	fmt.Println("Size: ", info.Size())
-	fmt.Println("Mode: ", info.Mode())
-	fmt.Println("ModTime: ", info.ModTime())
-
 	distPath := path.Join(stockPath, info.Name())
 	dst, err := os.Create(distPath)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer dst.Close()
 
