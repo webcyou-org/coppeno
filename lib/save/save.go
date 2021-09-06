fb3b0d29dbd462b0c540c6cf9cb33bd81e0bc05d
Daisuke Takayama <takayama_daisuke@cocone.co.jp>
2021-09-06 16:48:39 +0900

    feat: 🎸 add save func

--- lib/save/save.go
+++ lib/save/save.go
@@ -1,11 +1,28 @@
 package save
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
 func Start(key string, targetPath string) error {
 	fmt.Println(key)
 	fmt.Println(targetPath)
+	a := map[string]interface{}{
+		"x": 123,
+		"y": "ABC",
+	}
+
+	f, err := os.Create("data_output.json")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	err = json.NewEncoder(f).Encode(a)
+	if err != nil {
+		return err
+	}
 	return nil
 }
