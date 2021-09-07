8870703d31ba88f46578d7316cc24e487092e50a
Daisuke Takayama <takayama_daisuke@cocone.co.jp>
2021-09-07 17:16:10 +0900

    refactor: 💡 save func

--- lib/save/save.go
+++ lib/save/save.go
@@ -6,12 +6,12 @@ import (
 	"os"
 )
 
-func Start(key string, targetPath string) error {
-	fmt.Println(key)
+func Start(name string, targetPath string) error {
+	fmt.Println(name)
 	fmt.Println(targetPath)
 	a := map[string]interface{}{
-		"x": 123,
-		"y": "ABC",
+		"name": name,
+		"url": targetPath,
 	}
 
 	f, err := os.Create("data_output.json")
