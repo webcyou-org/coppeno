e9ed89422c1247d653165e2e93ae1b14bb2fbdc6
Daisuke Takayama <takayama_daisuke@cocone.co.jp>
2021-09-09 16:12:16 +0900

    refactor: 💡 save func

--- lib/load/load.go
+++ lib/load/load.go
@@ -3,6 +3,7 @@ package load
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -11,27 +12,36 @@ type Coppeno struct {
 	Url string  `json:"url"`
 }
 
-func Start(fileName string, fileGroup string) []*Coppeno {
+func Start(fileName string, fileGroup string) []Coppeno {
 	fmt.Println("load: Start")
-	f, _ := os.Open("coppeno.json")
-	//if err != nil {
-	//	return err
-	//}
-	defer f.Close()
+	//f, _ := os.Open("coppeno.json")
+	////if err != nil {
+	////	return err
+	////}
+	//defer f.Close()
 
-	var coppeno []*Coppeno
-	json.NewDecoder(f).Decode(&coppeno)
+	//var coppeno []*Coppeno
+	//json.NewDecoder(f).Decode(&coppeno)
 	//err = json.NewDecoder(f).Decode(&coppeno)
 	//if err != nil {
 	//	return err
 	//}
 
+	raw, err := ioutil.ReadFile("coppeno.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var coppeno []Coppeno
+	json.Unmarshal(raw, &coppeno)
+
 	//for _, v := range coppeno {
 	//	fmt.Printf("%+v\n", v)
 	//}
 	//// fmt.Printf("%+v\n", coppeno)
-	fmt.Println(fileName)
-	fmt.Println(fileGroup)
+	//fmt.Println(fileName)
+	//fmt.Println(fileGroup)
 	//return nil
 	return coppeno
 }
--- lib/save/save.go
+++ lib/save/save.go
@@ -7,16 +7,39 @@ import (
 	"webcyou-org/coppeno/lib/load"
 )
 
+type Coppeno struct {
+	Name string `json:"name"`
+	Url string  `json:"url"`
+}
+
 func Start(name string, targetPath string) error {
+	fmt.Println("save: Start")
+
 	coppeno := load.Start("fileName", "fileGroup")
 
+	// fmt.Println(len(coppeno))
 	fmt.Println(coppeno)
-	fmt.Println(name)
-	fmt.Println(targetPath)
-	a := map[string]interface{}{
-		"name": name,
-		"url": targetPath,
+	//fmt.Println(name)
+	//fmt.Println(targetPath)
+
+	for _, c := range coppeno {
+		// fmt.Sprintf("%s: %s", c.Name, c.Url)
+		fmt.Println(c.Name)
+		fmt.Println(c.Url)
 	}
+
+	//a := map[string]interface{}{
+	//	"name": name,
+	//	"url": targetPath,
+	//}
+
+	a := Coppeno{
+		Name: name,
+		Url: targetPath,
+	}
+	fmt.Println(a)
+
+	//coppeno = append(coppeno, a)
 
 	f, err := os.Create("data_output.json")
 	if err != nil {
