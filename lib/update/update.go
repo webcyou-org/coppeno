1776938271ee954fd8d81f3ed2093b4892289184
panicdragon <d.takayama.web@gmail.com>
2021-10-22 01:56:03 +0900

    feat: 🎸 create update command

--- lib/update/update.go
+++ lib/update/update.go
@@ -3,12 +3,16 @@ package update
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"regexp"
+	"strings"
 )
 
 type Coppeno struct {
 	Name string `json:"name"`
-	Url string  `json:"url"`
+	Url  string `json:"url"`
 }
 
 func Start(fileName string, fileGroup string) error {
@@ -25,10 +29,39 @@ func Start(fileName string, fileGroup string) error {
 	}
 
 	for _, v := range coppeno {
-		fmt.Printf("%+v\n", v)
+		// fmt.Printf("%+v\n", v)
+		fmt.Println(v.Name)
+		fmt.Println(v.Url)
+
+		r := regexp.MustCompile("^https?://github.com")
+		if r.MatchString(v.Url) {
+			downloadUrl := r.ReplaceAllString(v.Url, "https://raw.githubusercontent.com")
+			downloadUrl = strings.Replace(downloadUrl, "/blob", "", 1)
+			if err := DownloadFile(v.Name, downloadUrl); err != nil {
+				panic(err)
+			}
+			fmt.Println(downloadUrl)
+		}
 	}
 	// fmt.Printf("%+v\n", coppeno)
-	fmt.Println(fileName)
-	fmt.Println(fileGroup)
+	// fmt.Println(fileName)
+	// fmt.Println(fileGroup)
 	return nil
+}
+
+func DownloadFile(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
